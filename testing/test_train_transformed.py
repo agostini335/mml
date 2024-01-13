10f79fa8fe02a9e19c1cd42411a91b3b7dc3d315
andrea agostini <anandrea@ethz.ch>
2024-01-13 14:53:26 +0100

    training pipeline resnet init

--- testing/test_train_transformed.py
+++ testing/test_train_transformed.py
@@ -27,11 +27,28 @@ transformList = [transforms.ToTensor()]
 
 transform = transforms.Compose(transformList)
 
+
+# transform train
+transformList = [transforms.RandomAffine(degrees=(0, 5), translate=(0.05, 0.05), shear=(5)),
+                 transforms.RandomHorizontalFlip(), transforms.ToTensor(),
+                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
+train_transform = transforms.Compose(transformList)
+
+
+
+# transform test
+transformList = [transforms.ToTensor(),
+                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
+test_transform = transforms.Compose(transformList)
+
+
+
+
 train_dataset = CXRDataset(label_name=cfg_dataset.class_names[0], labels_df=train_dict['labels'],
-                           images=train_dict['images'], transform=transform)
+                           images=train_dict['images'], transform=train_transform)
 
 test_dataset = CXRDataset(label_name=cfg_dataset.class_names[0], labels_df=test_dict['labels'],
-                          images=test_dict['images'], transform=transform)
+                          images=test_dict['images'], transform=test_transform)
 
 # create dataloaders
 train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
