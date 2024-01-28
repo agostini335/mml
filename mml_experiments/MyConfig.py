c4ae43d7a5bc3aa8341232a409af6f00e89d7036
andrea agostini <anandrea@ethz.ch>
2024-01-28 20:38:17 +0100

    AP + PA

--- mml_experiments/MyConfig.py
+++ mml_experiments/MyConfig.py
@@ -16,13 +16,15 @@ class MIMICDatasetConfig(DatasetConfig):
     name: str = "MIMIC-CXR"
     root_dir_PA: str = "/Users/ago/PycharmProjects/mml/data/PA/mimic-cxr-preprocessed-16012024"
     root_dir_AP: str = "/Users/ago/PycharmProjects/mml/data/AP/mimic-cxr-preprocessed-17012024"
+    root_dir_split: str = "/Users/ago/PycharmProjects/mml/data"
     trans_resize: int = 224
     augmentation: str = "imagenet_style"
+    reduced_size: bool = True
 
 
 @dataclass
 class ExperimentConfig:
-    # task not used at the moment
+    # binary_classification
     task: str = "binary_classification"
     # experiment name
     name: str = "experiment"
@@ -30,20 +32,36 @@ class ExperimentConfig:
     target_list: List[str] = field(default_factory=lambda: ["Pneumonia"])
     # labeling policy
     label_policy: str = "uncertain_to_negative"
-    # viewPosition
-    view_position: str = "PA"
-    # splitting method
-    splitting_method: str = "random"
+    # viewPosition PA, AP, FRONTAL
+    view_position: str = "FRONTAL"
+    # splitting method random, original
+    splitting_method: str = "original"
     # splitting seed
     seed: int = 42
-    # train val split ratio
+    # train val split ratio, ignored if splitting method is original
     train_val_split: float = 0.6
-    # test val split ratio
+    # test val split ratio, ignored if splitting method is original
     test_val_split: float = 0.5
     # checkpoint metric
     checkpoint_metric: str = "val_auc"
     # checkpoint mode
     checkpoint_mode: str = "max"
+
+@dataclass
+class MultiLabelExperimentConfig(ExperimentConfig):
+    task: str = "multi_label_classification"
+    # experiment name
+    name: str = "experiment"
+    # target columns
+    target_list: List[str] = field(default_factory=lambda: ["Pneumonia"])
+    # labeling policy
+    label_policy: str = "uncertain_to_negative"
+    # splitting method
+    splitting_method: str = "original"
+    # viewPosition
+    view_position: str = "FRONTAL"
+
+
 
 @dataclass
 class ModelConfig:
@@ -74,13 +92,13 @@ class LogConfig:
     # wandb
     wandb_entity: str = "aa335"
     wandb_group: str = "mml"
-    wandb_run_name: str = "mml"
+    # wandb_run_name: str = "mml"
     wandb_project_name: str = "mml"
     wandb_log_freq: int = 50
-    wandb_offline: bool = True
+    wandb_offline: bool = False
 
     # logs
-    dir_logs: str = "/Users/ago/PycharmProjects/mml/logs"
+    dir_logs: str = "../logs"
 
 
 @dataclass
